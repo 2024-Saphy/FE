c19ba33b1e964b770ec505628eb3f8fc2cdc4f0f
2EETY <lty9251@naver.com>
2024-08-07 17:18:04 +0900

    setting: 리액트 초기세팅(ESLint, Prettier, TailwindCSS) - #1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header>
+        <h1>Hi</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
